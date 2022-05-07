95f656526c7c30efb58aca84803f5097555d2e16
Ivan Lavrenov <lavrenov@adv.ru>
2022-05-07 12:41:30 +0300

    Start game with submition

--- Scripts/SceneManager.gd
+++ Scripts/SceneManager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var next_scene = null
+var game_scene = null
 var spawn_teleport = null
 onready var player = load('res://Scenes/Player.tscn').instance()
 
@@ -12,6 +13,24 @@ func _ready():
 		player, $CurrentScene/Map_0/Teleport_0/SpawnPoint
 	)
 	$AnimationPlayer.play("FadeToNormal")
+
+func transition_to_game(scene):
+	game_scene = scene
+	game_scene.connect("win_game", self, "_on_game_exited")
+	game_scene.connect("exit_game", self, "_on_game_exited")
+	player.set_physics_process(false)
+	$AnimationPlayer.play("FadeToBlackGame")
+
+func transition_to_game_finished():
+	get_parent().add_child(game_scene)
+	$AnimationPlayer.play("FadeToNormal")
+	
+
+func _on_game_exited():
+	player.set_physics_process(true)
+	game_scene.queue_free()
+	$AnimationPlayer.play("FadeToNormal")
+
 
 
 func transition_to_fading(new_scene: String, teleport: String):
